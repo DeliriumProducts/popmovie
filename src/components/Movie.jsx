0bc5676a36f8da1c364d0a3afbb2b7c3775e2d5f
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-06-13 00:04:27 +0300

    Create Movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,1 +1,36 @@
-function Movie() {}
+import styled from '@emotion/styled';
+import { Card } from 'antd';
+import { Button, Icon } from 'antd';
+
+const MovieCard = styled(Card)`
+  border: none;
+  width: 250px;
+`;
+
+export default function Movie({ title, image }) {
+  return (
+    <>
+      <MovieCard
+        hoverable
+        cover={<img style={{ objectFit: 'cover' }} alt={title} src={image} />}
+      >
+        <Card.Meta
+          title={
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+              }}
+            >
+              <span>{title}</span>
+              <Button type="primary" size="small">
+                Watch now!
+              </Button>
+            </div>
+          }
+        />
+      </MovieCard>
+    </>
+  );
+}
