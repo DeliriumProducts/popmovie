bc6b0d8774b13c503ae3105cd204b3f2ccc182d7
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-06-12 23:14:24 +0300

    Update ant vars

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,4 +4,21 @@ import Link from 'next/link';
 import { keyframes } from '@emotion/core';
 import { THEME_VARIABLES } from '../config/env';
 
-export default () => <h1>Hi</h1>;
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
+export default () => (
+  <Card
+    hoverable
+    style={{ width: 240 }}
+    cover={
+      <img
+        alt="example"
+        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+      />
+    }
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+);
