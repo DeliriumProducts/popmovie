37c5b5c634ebcb6a64c080bae50490c4607fdda3
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-06-12 22:48:22 +0300

    Make Layout

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,38 @@
 import App, { Container } from 'next/app';
 import Router from 'next/router';
 import { Global, css } from '@emotion/core';
+import styled from '@emotion/styled';
 import ContextProvider from '../context/providers/contextProvider.jsx';
 import Head from 'next/head';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 import React from 'react';
 import NProgress from 'nprogress';
 import '../assets/nprogress.less';
 
 const { Header, Content, Footer } = Layout;
+const { Item } = Menu;
 
 Router.events.on('routeChangeStart', () => {
   NProgress.start();
 });
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledLayout = styled(Layout)`
+  min-height: 100vh;
+`;
+
+const StyledContent = styled(Content)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -60,13 +78,20 @@ export default class MyApp extends App {
             `}
           />
           <Container>
-            <Layout>
-              <Header> The largest movie database ever.</Header>
-              <Content>
+            <StyledLayout>
+              <Header>
+                <HeaderContainer>
+                  <h1>The largest movie database ever.</h1>{' '}
+                  <Menu mode="horizontal" theme="dark">
+                    <Item key="login">Login with Google!</Item>
+                  </Menu>
+                </HeaderContainer>
+              </Header>
+              <StyledContent>
                 <Component {...pageProps} />
-              </Content>
-              <Footer>Footer</Footer>
-            </Layout>
+              </StyledContent>
+              <Footer>Delirium Products! ©2019</Footer>
+            </StyledLayout>
           </Container>
         </ContextProvider>
       </>
