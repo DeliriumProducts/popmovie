0a13b365cd6f0fc9980fe96dbea04847b06b7517
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-06-13 16:24:38 +0300

    Show modal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import styled from '@emotion/styled';
 import MovieCard from '../components/Movie';
+import CreateButton from '../components/CreateButton';
+import { Modal } from 'antd';
 
 const Container = styled.div`
   display: flex;
@@ -12,47 +15,62 @@ const Container = styled.div`
   }
 `;
 
-export default () => (
-  <Container>
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-    <MovieCard
-      title="Iron Man 3"
-      image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
-    />
-  </Container>
-);
+export default () => {
+  const [modalVisible, setModalVisible] = React.useState(false);
+  return (
+    <Container>
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <MovieCard
+        title="Iron Man 3"
+        image="https://s1.thcdn.com/productimg/1600/1600/10808803-5034529881593765.jpg"
+      />
+      <CreateButton onClick={() => setModalVisible(true)} />
+      <Modal
+        title="Vertically centered modal dialog"
+        centered
+        visible={modalVisible}
+        onOk={() => setModalVisible(false)}
+        onCancel={() => setModalVisible(false)}
+      >
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
+    </Container>
+  );
+};
